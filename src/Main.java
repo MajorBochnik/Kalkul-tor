beb2508d2ec67e0e3214c63bc8540db68d0d0913
MajorBochnik <smidav20@gasos-ro.cz>
2025-01-06 12:54:42 +0100

    Kalkulátor

--- src/Main.java
+++ src/Main.java
@@ -16,28 +16,29 @@ public class Main {
         System.out.println("6: Ukonční programu");
 
         int cislo = sc.nextInt();
+        sc.nextLine();
 
 
         switch (cislo) {
             case 1:
                 System.out.println("Zadej 1. řetězec:");
-                String veta1 = sc.next();
+                String veta1 = sc.nextLine();
                 System.out.println("Zadej 2. řetězec:");
-                String veta2 = sc.next();
+                String veta2 = sc.nextLine();
                 System.out.println(veta1 + veta2);
 
                 break;
             case 2:
                 System.out.println("Napiš řetězec:");
-                String veta3 = sc.next();
+                String veta3 = sc.nextLine();
                 System.out.println("Tento řetězec má: " + veta3.length() + " znaků.");
 
                 break;
             case 3:
                 System.out.println("Napiš řetězec:");
-                String veta4 = sc.next();
+                String veta4 = sc.nextLine();
                 System.out.println("Chceš převést na: Velká/Malá");
-                String veta5 = sc.next();
+                String veta5 = sc.nextLine();
                 if (veta5.equals("Velká")) {
                     System.out.println(veta4.toUpperCase());
                 } else {
@@ -48,8 +49,10 @@ public class Main {
             case 4:
                 System.out.println("Zadej spodní hranici intervalu:");
                 int cislo1 = sc.nextInt();
+                sc.nextLine();
                 System.out.println("Zadej horní hranici intervalu:");
                 int cislo2 = sc.nextInt();
+                sc.nextLine();
                 Random rnd = new Random();
                 System.out.println(rnd.nextInt(cislo1, cislo2));
 
@@ -57,6 +60,7 @@ public class Main {
             case 5:
                 System.out.println("Zadej číslo:");
                 int cislo3 = sc.nextInt();
+                sc.nextLine();
                 System.out.println("Jeho druhá mocnina je: " + Math.pow(cislo3, 2));
 
                 break;
